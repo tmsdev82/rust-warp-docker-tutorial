04f4940eec370df81ac4c88da790691a44560920
tim <tmsteem82@gmail.com>
2021-12-10 22:39:53 +0100

    implement read from env var

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::env;
 use std::fs;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     println!("Starting server...");
+    let instance_name = env::var("INSTANCE_NAME").expect("Error reading environment variable.");
     let text_msg = fs::read_to_string("data/data.txt").expect("could not read file");
+    let text_msg = format!("[{}] {}", instance_name, text_msg);
     let root = warp::path::end().map(move || text_msg.clone());
 
     let routes = root.with(warp::cors().allow_any_origin());
